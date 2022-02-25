e2fb56f24b42e3cfce82ff43e2d451c4a1ddb0a8
Sojborg <sojborg@outlook.com>
2022-02-25 17:17:23 +0100

    only return index.html on / route

--- server/server.ts
+++ server/server.ts
@@ -18,6 +18,6 @@ app.get("/api/hello", (req, res) => {
 });
 
 // All other unmatched requests will return the React app
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
 });
